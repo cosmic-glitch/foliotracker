1fec8d3f443ddc30f97616789c473b9a32f9abf3
cosmic-glitch <cosmic.glitch@gmail.com>
2026-01-09 16:34:50 -0800

    Move By Type panel below Holdings panel

--- src/App.tsx
+++ src/App.tsx
@@ -164,14 +164,8 @@ function App() {
               currentValue={data.totalValue}
               marketStatus={data.marketStatus}
             />
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-              <div className="lg:col-span-2">
-                <HoldingsTable holdings={data.holdings} />
-              </div>
-              <div className="lg:col-span-1">
-                <HoldingsByType holdings={data.holdings} />
-              </div>
-            </div>
+            <HoldingsTable holdings={data.holdings} />
+            <HoldingsByType holdings={data.holdings} />
           </>
         ) : null}
       </main>
